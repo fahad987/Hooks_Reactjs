2bbc3651028eafbdf4d4c91ffa9ddd0875492edb
Fahad Rehman <fahad.rehman365@yahoo.com>
2020-07-16 15:44:53 +0500

    Update Digitial CLock

--- src/App.js
+++ src/App.js
@@ -1,58 +1,20 @@
-// import React, { useState } from 'react';
-// import './App.css'
-
-// const App = () => {
-
-//   const state = useState();
-// const  [count,setCount]=useState(0)
-
-// const IncNum = () => {
-//   setCount(count + 1)
-// }
-
-//   return(
-//     <div>
-//       <h1>{count}</h1>
-//       <button onClick={IncNum}> Click Me </button>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-// import React, {useState} from 'react'
-// import './App.css'
-
-// const App = () => {
-    
-//     let newTime = new Date().toLocaleTimeString();
-//     const [cTime, setTime] = useState(newTime)
-
-// const UpdateTime = () => {
-// newTime = new Date().toLocaleTimeString();
-// setTime(newTime)
-// }
-
-//     return(
-// <div>
-// <h1> {cTime}</h1>
-// <button onClick={UpdateTime}> get time</button>
-// </div>
-//     )
-// }
-// export default App;
-
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+
 const App = () => {
- 
- let Time = new Date().toLocaleTimeString();
- 
+
+let time = new Date().toLocaleTimeString();
+
+const [ctime, cUpdateTime] = useState(time);
+
+const updateTime = () => {
+time = new Date().toLocaleTimeString();
+cUpdateTime(time);
+}
+setInterval(updateTime, 1000)
   return(
-    <div>
-      <h1>  {Time}</h1>
-    </div>
+    <h1>{ctime}</h1>
   )
 }
 
