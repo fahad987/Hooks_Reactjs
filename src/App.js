4f972d59a2aea97a3d46c54c9bdba3b3dd06febb
Fahad Rehman <fahad.rehman365@yahoo.com>
2020-07-16 14:31:17 +0500

    Hooks use For Practice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { useState } from 'react';
+// import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const App = () => {
+
+//   const state = useState();
+// const  [count,setCount]=useState(0)
+
+// const IncNum = () => {
+//   setCount(count + 1)
+// }
+
+//   return(
+//     <div>
+//       <h1>{count}</h1>
+//       <button onClick={IncNum}> Click Me </button>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React, {useState} from 'react'
+import './App.css'
+
+const App = () => {
+    
+    let newTime = new Date().toLocaleTimeString();
+    const [cTime, setTime] = useState(newTime)
+
+const UpdateTime = () => {
+newTime = new Date().toLocaleTimeString();
+setTime(newTime)
 }
 
+    return(
+<div>
+<h1> {cTime}</h1>
+<button onClick={UpdateTime}> get time</button>
+</div>
+    )
+}
 export default App;
